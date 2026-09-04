3b80391caa2967ee9b36ac09fc204eeb5180ef3a
Kane Blueriver <kxxoling@gmail.com>
2026-09-04 17:29:43 +0800

    chore: bump version to 0.4.1 (versionCode 6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,8 +88,8 @@ android {
         applicationId = "app.floatdeck"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.4.0"
+        versionCode = 6
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
